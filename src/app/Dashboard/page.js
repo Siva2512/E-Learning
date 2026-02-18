e2ca2dd09f30e8a076ecdb8e66e8a21bea1e5eb9
Siva2512 <sivagpoomari@gmail.com>
2026-02-18 18:44:21 +0530

    created the dashboard page

--- src/app/Dashboard/page.js
+++
@@ -1,5 +0,0 @@
-// import Dashboard from "../Pages/Student/Dashboard";
-
-// export default function Page() {
-//   return <Dashboard />;
-// }
--- src/app/Student/page.js
+++ src/app/Student/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Dashboard from '../Pages/Student/Dashboard'
 
+
 export default function page() {
   return (
    <Dashboard/>
