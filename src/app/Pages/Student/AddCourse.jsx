1fe4f230d346d54612f78d5097226cb3a421289c
Siva2512 <sivagpoomari@gmail.com>
2026-03-09 17:43:18 +0530

    created redux for add course

--- src/app/Pages/Student/AddCourse.jsx
+++ src/app/Pages/Student/AddCourse.jsx
@@ -37,7 +37,7 @@ export default function AddCourse() {
       ...course,
     };
 
-    // 👉 Add course to Redux
+    //  Add course to Redux
     dispatch(addCourse(newCourse));
 
     // redirect
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import courseReducer from "./courseSlice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    courses: courseReducer,
-  },
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  courses: courseReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
